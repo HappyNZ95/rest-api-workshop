e4051d93575486f53f6fb13f2bcb7a6fe67c749a
HappyNZ95 <htewini@gmail.com>
2024-02-21 16:17:56 +1300

    created test customer for Mongo Atlas

--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
 let express = require('express')
-
 let app = express()
-
 let personRoute = require('./routes/person')
-
+let customerRoute = require('./routes/customer')
 let path = require('path')
+let bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+/*//Mongoose connection
+
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://happynz:Nbe0b4JOg5CDPw9Q@cluster0.1zlf2lw.mongodb.net/?retryWrites=true&w=majority";
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await mongoose.connect(uri);
+    
+    // If connection is successful
+    console.log("You successfully connected to MongoDB!");
+
+  } catch (error) {
+    // If there's an error while connecting
+    console.error('Error connecting to mongodb', error);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.connection.close();
+  }
+}
+
+run();*/
 
 // middleware
 app.use((req, res, next) => {
-    console.log(`${new Date().toString()} => ${req.originalUrl}`)
+    console.log(`${new Date().toString()} => ${req.originalUrl}`, req.body)
     next()
 })
 
 app.use(personRoute)
+app.use(customerRoute)
 app.use(express.static('public'))
 
-//Handler for 404 - Resource Not Found
+//Handler for 404 - Resource Not Found //
 app.use((req, res, next) => {
     res.status(404).send('We think you are lost!')
 })
--- src/models/customer.model.js
+++ src/models/customer.model.js
@@ -1,6 +1,14 @@
 let mongoose = require('mongoose')
+const uri = "mongodb+srv://happynz:Nbe0b4JOg5CDPw9Q@cluster0.1zlf2lw.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(uri);
 
-const server = ''
-const database = ''
-const user = 'happynz'
-const password = 'Nbe0b4JOg5CDPw9Q'
+let CustomerSchema = new mongoose.Schema({
+    name: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+})
+
+module.exports = mongoose.model('Customer', CustomerSchema)
